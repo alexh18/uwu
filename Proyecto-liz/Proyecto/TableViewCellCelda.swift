8af4d31a7bad0317954e36a47f55f65ab0d0171c
alexh18 <99298296+alexh18@users.noreply.github.com>
2022-06-07 08:43:38 -0500

    Add files via upload

---
+++ Proyecto-liz/Proyecto/TableViewCellCelda.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCellCelda.swift
+//  Proyecto
+//
+//  Created by Daniel Belmont on 26/04/22.
+//
+
+import UIKit
+
+class TableViewCellCelda: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Proyecto-liz/Proyecto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto
+//
+//  Created by Daniel Belmont on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
